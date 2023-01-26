905c12fcbeab1cac152ee608946776b038df343c
Victor M. Varela <vmvarela@gmail.com>
2023-01-26 13:06:44 +0100

    prueba teams

--- area-1/main.tf
+++ area-1/main.tf
@@ -1,1 +1,14 @@
 # tf area-1
+
+module "jefe-area-1-a" {
+  source  = "../modules/team"
+
+  name        = "area-1-jefes"
+  description = "Jefes Area 1"
+  privacy     = "secret"
+
+  members = [
+    "vmvarela-jefearea"
+  ]
+
+}
